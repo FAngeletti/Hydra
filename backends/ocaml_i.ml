ab85b49163a1440380ca00d4ba73556b043d4d92
Florian Angeletti <angeletti@achronie.fr>
2014-10-06 23:52:32 +0200

    Fully featured ocaml interpreter mode TODO : Simplify nesting rule for captured string.

--- backends/ocaml_i.ml
+++ backends/ocaml_i.ml
@@ -6,64 +6,87 @@ open Lexer
 
 type interpreter = { out : out_channel; inp : in_channel }
 
+type env = {target: string ; ocaml : interpreter; mutable seq: bool } 
 
-type env = { target :out_channel ;  ocaml: interpreter    }
+let send_write env s = 
+	output_string env.ocaml.out s
 
-let write env s= output_string env.target s
- 
+let send env s = 
+	env.seq <- false ; send_write env s
+
+let trim s=
+	let len = String.length s - 1 in 
+	let rec forward n s = if n >= len then n else
+	match s.[n] with 
+		| ' ' | '#' -> forward (n+1) s
+		| _ -> n 
+	in
+	let n = forward 0 s in
+	String.sub s n (len-n+1)   
+
+let read_ocaml env =
+ let input = ref "" in
+while !input <> "Exception: End_of_file."  do
+(* for i=1 to 2 do *) 
+	if !input <> "- : unit = ()" && !input <> "\n" then Printf.printf "%s\n" !input;
+	let s = trim @@ input_line env.ocaml.inp in
+	input:= s
+done
+
+let flush_i env =  (* send env "raise End_of_file;;\n"; *) flush env.ocaml.out (*; read_ocaml env *)
+
+let eval env =  send env ";;\n" ; flush_i env; env.seq <- false 
+
+
+let preambule env = let c = Printf.sprintf
+"
+module Hydra = struct 
+let f = open_out \"%s\" 
+
+let context = ref []
+
+let write s= match !context with
+  |[] -> output_string f s
+  | b :: q -> Buffer.add_string b s
+
+let new_context ()= 
+  let b=Buffer.create 80 in
+  context := b:: !context  
+
+let pop_context () = match !context with
+  | [] -> ()
+  | b::q -> context:=q; Buffer.contents b |> write
+end
+"
+env.target in
+send env c; eval env 
+
+
+
+let write env s=  if env.seq then send_write env ";"  else env.seq <- true ;  send_write env @@ "Hydra.write ("^s^")\n"
+let write_text env s=  write env ("\"" ^ s ^"\"")  
 
 let init_env basename = 
-	let target  = basename |> open_out 
-	and inp, out = Unix.open_process "ocaml" in
-	let () = ignore [input_line inp; input_line inp] in
-	{ target ; ocaml = {out;inp} }
-
-let preambule env = ()
-
-let discard {inp;_} = while input_line inp <> "# Exception: End_of_file." do () done
-
-let search char s start  =
-	let rec search start = 
-	match start= String.length s with
-		| true -> raise Not_found
-		| false -> if s.[start] = char then start else search @@ start+1 in
-	search start 
+	let target  = basename
+	and inp, out =   (*open_in "ocaml.inpt", open_out "ocaml.out"*)  Unix.open_process "ocaml" in
+	{ target ; ocaml = {out;inp}; seq=false }
 
 
-exception Type_error of string
-exception Not_implemented of string
 
-let send ocaml s = 
-	output_string ocaml.out s ; flush ocaml.out
-
-let parse_toplevel ocaml =
-	let s = input_line ocaml.inp in
-	let type_start= search ':' s 0 in
-	let type_stop = search '=' s (type_start) in
-	let type_s = String.trim @@ String.sub s (type_start+1) (type_stop-type_start-1) in
-	match type_s with
-		| "string" | "bytes" -> begin    
-		let start = search '"' s type_stop in
-		let stop = search  '"' s (start+1) in
-		String.sub s (start+1) (stop-start-1) 
-		end
-		| s -> raise @@ Type_error type_s
-
-let eval ocaml s = 
-	send ocaml (s^";;\n"); 
-	parse_toplevel ocaml
 
 let write_text env kind s = 
 	match kind with
-	| Text | Capture -> write env s 
-	| Code -> List.iter (send env.ocaml) [s; ";;\n"; "let _ = raise End_of_file;;\n"] ;  discard env.ocaml 
-	| Inclusion -> let m = eval env.ocaml s in write env m 
+	| Text -> write_text env s ; eval env 
+	| Code -> send env s
+	| Capture -> write_text env s
+	| Inclusion -> write env s 
 			
 let write_node env kind hydres k = match kind with
-	| Capture ->  raise @@ Not_implemented "Capture in interpreted mode" 
+	| Code -> k env hydres ; eval env 
+	| Capture ->  send env "begin\nHydra.new_context ()\n"; env.seq<-true; k env hydres;  send env "; Hydra. pop_context ()\nend\n"  
 	| _ -> k env hydres
 			 
-let end_compilation {ocaml;_} = ignore ( Unix.close_process (ocaml.inp,ocaml.out) )
+let end_compilation env = send env "flush Hydra.f"; eval env ; ignore ( Unix.close_process (env.ocaml.inp,env.ocaml.out) )
   
 
 let backend = transcompile {
@@ -71,5 +94,6 @@ let backend = transcompile {
 	preambule;
 	write_node;
 	write_text;
-	end_compilation}
+	end_compilation
+}
 
